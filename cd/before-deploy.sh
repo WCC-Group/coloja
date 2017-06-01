1dc83278542bcb72d8a251b0f20c31de0b42b055
ekoopman <ekoopman@wcc-group.com>
2017-06-01 17:06:44 +0200

    Fix codesigning file location

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-
-  	openssl aes-256-cbc -K $encrypted_46b4bbc90abb_key -iv $encrypted_46b4bbc90abb_iv -in codesigning.asc.enc -out codesigning.asc -d
+  	openssl aes-256-cbc -K $encrypted_46b4bbc90abb_key -iv $encrypted_46b4bbc90abb_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
 
     gpg --fast-import cd/signingkey.asc
 fi
